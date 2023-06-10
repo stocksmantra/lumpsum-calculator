d1bd94007f06687e62e83753656e47730889d31c
rahulkrsing <rahulkr.cotocus@gmail.com>
2023-06-10 15:57:17 +0530

    updating lumpsum calculator

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function calculateLumpSum() {
-    const initialAmount = parseFloat(document.getElementById("initial_amount").value);
-    const interestRate = parseFloat(document.getElementById("interest_rate").value);
-    const years = parseInt(document.getElementById("years").value);
-  
-    const lumpSum = initialAmount * Math.pow(1 + interestRate / 100, years);
-  
-    document.getElementById("result").value = lumpSum.toFixed(2);
-  }
+
+function calculateLumpsum() {
+  // Get input values
+  const investedAmount = parseFloat(document.getElementById("investedAmount").value);
+  const expectedReturns = parseFloat(document.getElementById("expectedReturns").value) / 100;
+  const investmentPeriod = parseFloat(document.getElementById("investmentPeriod").value);
+
+  // Calculate total value using compound interest formula
+  const totalValue = investedAmount * Math.pow((1 + expectedReturns), investmentPeriod);
+
+  // Calculate estimated returns
+  const estimatedReturns = totalValue - investedAmount;
+
+  // Format the results as Indian Rupees
+  const formatOptions = { style: 'currency', currency: 'INR', minimumFractionDigits: 2, maximumFractionDigits: 2 };
+  const formattedInvestedAmount = investedAmount.toLocaleString('en-IN', formatOptions);
+  const formattedEstimatedReturns = estimatedReturns.toLocaleString('en-IN', formatOptions);
+  const formattedTotalValue = totalValue.toLocaleString('en-IN', formatOptions);
+
+  // Display results
+  document.getElementById("resultInvestedAmount").textContent = formattedInvestedAmount;
+  document.getElementById("resultEstimatedReturns").textContent = formattedEstimatedReturns;
+  document.getElementById("resultTotalValue").textContent = formattedTotalValue;
+}
